1e2ce93bac82911a847e8f1978333602cd801524
Wiktoria Kulczynska <wiktoriakulczynska@gmail.com>
2023-07-12 12:02:09 +0200

    init project

---
+++ src/main/java/com/wildevent/WildEventMenager/WildEventMenagerApplication.java
@@ -0,0 +1,13 @@
+package com.wildevent.WildEventMenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildEventMenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildEventMenagerApplication.class, args);
+	}
+
+}
