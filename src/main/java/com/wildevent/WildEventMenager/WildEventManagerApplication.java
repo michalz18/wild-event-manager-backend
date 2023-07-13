1438c7fe4d4e557b9e242bd824d5a11b03a3785f
Wiktoria Kulczynska <wiktoriakulczynska@gmail.com>
2023-07-13 09:10:09 +0200

    init H2 Database

---
+++ src/main/java/com/wildevent/WildEventMenager/WildEventManagerApplication.java
@@ -0,0 +1,14 @@
+package com.wildevent.WildEventMenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildEventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildEventManagerApplication.class, args);
+	}
+
+}
+
