1cf5b9afecfbed6b90d34e8dad30f8a52870027c
Michał Zuzak <michalzuzak.2@gmail.com>
2023-07-26 14:51:34 +0200

    WILD-3 Feature Getting events for today implemented and added the management of HTTP responses using ResponseEntity

--- src/main/java/com/wildevent/WildEventMenager/event/controller/EventController.java
+++ src/main/java/com/wildevent/WildEventMenager/event/controller/EventController.java
@@ -3,6 +3,7 @@ package com.wildevent.WildEventMenager.event.controller;
 import com.wildevent.WildEventMenager.event.model.EventTitleDTO;
 import com.wildevent.WildEventMenager.event.service.EventServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class EventController {
     }
 
     @GetMapping("/today")
-    public List<EventTitleDTO> getTodayEvents() {
-        return eventService.getTodayEvents();
+    public ResponseEntity<List<EventTitleDTO>> getTodayEvents() {
+        return ResponseEntity.ok().body(eventService.getTodayEvents());
     }
 }
