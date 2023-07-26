7a9d2a39d48a4c25f50161a0ec2cdd9518c6ac68
Wiktoria Kulczynska <wiktoriakulczynska@gmail.com>
2023-07-26 11:04:54 +0200

    WILD-3 Map GET endpoint is completed

---
+++ src/main/java/com/wildevent/WildEventMenager/map/controller/MapController.java
@@ -0,0 +1,33 @@
+package com.wildevent.WildEventMenager.map.controller;
+
+import com.wildevent.WildEventMenager.map.repository.MapRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("no-auth/map")
+public class MapController {
+
+    private final MapRepository mapRepository;
+
+    public MapController(MapRepository mapRepository) {
+        this.mapRepository = mapRepository;
+    }
+
+    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage() {
+        try {
+            byte[] mapData = mapRepository.getMap();
+            return ResponseEntity.ok().body(mapData);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
