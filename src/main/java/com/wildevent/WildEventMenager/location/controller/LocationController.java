a2db52043cefe177ba937241a513809d65f9be74
apoko <apokora@itsap.pl>
2023-07-27 09:58:38 +0200

    WILD-2 Add @CrossOrigin annotation

--- src/main/java/com/wildevent/WildEventMenager/location/controller/LocationController.java
+++ src/main/java/com/wildevent/WildEventMenager/location/controller/LocationController.java
@@ -4,16 +4,13 @@ import com.wildevent.WildEventMenager.location.model.LocationDTO;
 import com.wildevent.WildEventMenager.location.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/location")
+@CrossOrigin(origins = "http://localhost:3000")
 public class LocationController {
     private final LocationService locationService;
 
