c26a827da9a761a01354a74d52506bfdc515e751
marcinlewinski <marcinlewinski71@gmail.com>
2023-08-22 22:32:39 +0200

    WILD-51-3 Get all locations endpoint has been implemented

---
+++ src/main/java/com/wildevent/WildEventMenager/location/model/dto/LocationIdTitleDTO.java
@@ -0,0 +1,7 @@
+package com.wildevent.WildEventMenager.location.model.dto;
+
+import java.util.UUID;
+
+public record LocationIdTitleDTO(UUID id, String title) {
+
+}
--- src/main/java/com/wildevent/WildEventMenager/location/service/dtoMappers/LocationDTOMapper.java
+++ src/main/java/com/wildevent/WildEventMenager/location/service/dtoMappers/LocationDTOMapper.java
@@ -2,10 +2,12 @@ package com.wildevent.WildEventMenager.location.service.dtoMappers;
 
 import com.wildevent.WildEventMenager.location.model.Location;
 import com.wildevent.WildEventMenager.location.model.dto.LocationDTO;
+import com.wildevent.WildEventMenager.location.model.dto.LocationIdTitleDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface LocationDTOMapper {
 
     LocationDTO getLocationDtoFromLocation(Location location);
+    LocationIdTitleDTO getLocationIdTitleDTO(Location location);
 }
