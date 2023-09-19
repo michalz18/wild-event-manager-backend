fe89ae89dce5ba401989b211f503b0931fe4b330
Michał Zuzak <michalzuzak.2@gmail.com>
2023-09-19 21:43:06 +0200

    WILD-84 Extended AuthenticationResponse to return id of authenticated user

--- src/main/java/com/wildevent/WildEventMenager/security/auth/response/AuthenticationResponse.java
+++ src/main/java/com/wildevent/WildEventMenager/security/auth/response/AuthenticationResponse.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class AuthenticationResponse {
     private String token;
+    private String id;
     private String name;
     private String email;
     private Set<Role> roles;
